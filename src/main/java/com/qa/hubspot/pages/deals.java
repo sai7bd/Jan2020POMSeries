21f000809bcdf883f59c56fb5081cfdb8916a697
sai7bd <sai7bd@gmail.com>
2020-04-13 21:35:41 -0500

    Added new deals page class

---
+++ src/main/java/com/qa/hubspot/pages/deals.java
@@ -0,0 +1,15 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.base.BasePage;
+
+public class deals extends  BasePage{
+WebDriver driver;
+
+public  deals(WebDriver driver) {
+	this.driver = driver;
+}
+	
+	
+}
